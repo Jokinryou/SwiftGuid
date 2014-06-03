d51e50581f65f69acfc4d35a8fe3b9516862a807
Jokinryou Tsui <jokinryou.tsui@gmail.com>
2014-06-03 23:26:35 +0800

    initial commit

---
+++ section-1.swift
@@ -0,0 +1,45 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+
+// ------------- page 5 ---------------
+
+println("Hello, Swift~")
+
+// -------------- page 6 -----------------
+
+var myVariable = 42
+myVariable = 50
+
+let myConstant = 42
+
+// ---------------- page 7 -------------------
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+// --- experient --- 
+
+let explicitFloat: Float = 4
+
+
+// --------
+
+let label = "The width is "
+let width = 34
+let widthLabel = label + String(width)
+
+// --------------- page 8 ------------
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let myFloat = 3.3
+let name = "Jerry"
+
+let result = "Hello, \(name). \(myFloat)"
